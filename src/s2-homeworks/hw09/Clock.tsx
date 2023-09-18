9210ef4a61eba18ae2490e2f36a4eb0b1af42f9e
exleonardo <exleonardo@mail.ru>
2023-09-18 14:51:55 +0300

    HW 9 refactor

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -33,7 +33,12 @@ function Clock() {
     }
 
     const stringTime = (date.getHours()<10?'0' + date.getHours():date.getHours()) + ':' +(date.getMinutes()<10?'0'+date.getMinutes():date.getMinutes()) + ':'+ (date.getSeconds()<10?'0'+date.getSeconds():date.getSeconds())  || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = (date.getDay()<10?'0'+date.getDay():date.getDay())+'.'+(date.getMonth()<10?'0'+date.getMonth():date.getMonth())+'.'+(date.getFullYear()) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = new Date().toLocaleString('ru',
+        {
+            day: 'numeric',
+            month: 'numeric',
+            year: 'numeric'
+        }) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = date.toLocaleDateString("en-US",{
