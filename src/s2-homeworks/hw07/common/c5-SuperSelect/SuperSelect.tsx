71d08b0bb83b6aee5f80fbbb2a4e73d41dca774a
exleonardo <exleonardo@mail.ru>
2023-10-18 12:09:42 +0300

    HW 12

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -1,9 +1,10 @@
-import React, {
-    SelectHTMLAttributes,
-    DetailedHTMLProps,
-    ChangeEvent,
+import React , {
+    SelectHTMLAttributes ,
+    DetailedHTMLProps ,
+    ChangeEvent  ,
 } from 'react'
 import s from './SuperSelect.module.css'
+import {ThemesType} from "../../../hw12/HW12";
 
 type DefaultSelectPropsType = DetailedHTMLProps<
     SelectHTMLAttributes<HTMLSelectElement>,
@@ -11,8 +12,8 @@ type DefaultSelectPropsType = DetailedHTMLProps<
 >
 
 type SuperSelectPropsType = DefaultSelectPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
+    options?: ThemesType[]
+    onChangeOption?: (option: number) => void
 }
 
 const SuperSelect: React.FC<SuperSelectPropsType> = ({
@@ -22,13 +23,14 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
     onChangeOption,
     ...restProps
 }) => {
-    const mappedOptions: any[] = options
+    const mappedOptions: JSX.Element[]  = options
         ? options.map((o) => (
               <option
                   id={'hw7-option-' + o.id}
-                  className={s.option}
+                  className={s.option + o.id}
                   key={o.id}
                   value={o.id}
+
               >
                   {o.value}
               </option>
@@ -37,9 +39,11 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         // делают студенты
+
         onChangeOption?.(+e.currentTarget.value)
     }
 
+    // const finalSelectClassName = s.select + (className ? ' ' + className : '')
     const finalSelectClassName = s.select + (className ? ' ' + className : '')
 
     return (
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,14 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ChangeThemeIdAC): InitialState => { // fix any
     switch (action.type) {
-        // дописать
-
+        case "SET_THEME_ID":
+            return {...state,themeId: action.id}
         default:
             return state
     }
 }
-
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ChangeThemeIdAC = ReturnType<typeof changeThemeId>
+export type InitialState = typeof initState
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id }as const) // fix any
