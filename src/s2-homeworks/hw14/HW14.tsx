e0580a94e7f0450d10fb8a9ff8c1b70e94a4b0a3
exleonardo <exleonardo@mail.ru>
2023-11-09 23:29:05 +0300

    HW 14

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -35,9 +35,9 @@ const HW14 = () => {
         getTechs(value)
             .then((res) => {
                 // делает студент
-
+                setLoading(false)
                 // сохранить пришедшие данные
-
+                setTechs(res?.data.techs?res?.data.techs:[])
                 //
             })
     }
@@ -45,9 +45,8 @@ const HW14 = () => {
     const onChangeText = (value: string) => {
         setFind(value)
         // делает студент
-
         // добавить/заменить значение в квери урла
-        // setSearchParams(
+        setSearchParams(find)
 
         //
     }
